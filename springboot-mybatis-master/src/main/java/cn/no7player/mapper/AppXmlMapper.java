fff6d7acd9239c0626d8b8a1b64c9f765fe0b242
Qmrhan <1946533122@qq.com>
2017-12-18 13:20:51 +0800

    生成文件

--- springboot-mybatis-master/src/main/java/cn/no7player/mapper/AppXmlMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.no7player.mapper;
-
-import cn.no7player.model.AppXmlEntity;
-
-import java.util.Map;
-
-public interface AppXmlMapper {
-    public AppXmlEntity getAppXml(Map page);
-}
--- springboot-mybatis-master/src/main/java/cn/no7player/mapper/ApplyOperationMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.no7player.mapper;
-
-import cn.no7player.model.ApplyOperationEntity;
-
-import java.util.Map;
-
-public interface ApplyOperationMapper {
-    ApplyOperationEntity getApplyOperation(Map page);
-}
